26c7680f18780c5440a1b26c534aeae0a04c0aa8
Alby Destiny Litaoen <144974382+DLitaoen@users.noreply.github.com>
2024-10-12 19:58:40 +0800

    Renamed to conform to conventions

--- Loops.php
+++ Loops.php
@@ -10,7 +10,8 @@
 
     // https://primus-r.notion.site/Loops-83ee14b7653c47fd95d9f80348350c82
 
-    echo "\nActivity 1\n";
+    echo "Activity 1";
+    echo "<br>";
     $number = 1;
     while ($number <= 20) {
         if ($number % 2 == 0) {
@@ -19,33 +20,38 @@
         $number++;
     }
 
-    // CLI line break
-    echo "\n\n\n";
+    
+    echo "<br> <br>";
 
-    echo "Activity 2\n";
+    echo "Activity 2";
+    echo "<br>";
     $correctPassword = "password123";
-    do {
-        // echo "Please enter the password: ";
-        $userPassword = readline("\nPlease enter the password: \n");
-        if ($userPassword != "password123") {
-            echo "Incorrect password.\n";
-        }
-    } while ($userPassword != $correctPassword);
-    echo "Access Granted.\n";
 
-    // CLI line break
-    echo "\n\n";
+    echo "<i>Will loop endlessly in browser, so I commented it out for now.<i>";
 
-    echo "Activity 3\n";
+    // do {
+    //     // echo "Please enter the password: ";
+    //     $userPassword = readline("\nPlease enter the password: \n");
+    //     if ($userPassword != "password123") {
+    //         echo "Incorrect password.\n";
+    //     }
+    // } while ($userPassword != $correctPassword);
+    // echo "Access Granted.\n";
+
+    echo "<br> <br>";
+
+    echo "Activity 3";
+    echo "<br>";
     $multiplicand = 7;
     for ($i = 1; $i <= 10; $i++) {
         echo "7 x $i = " . $multiplicand * $i . "\n";
     }
 
-    // CLI line break
-    echo "\n\n";
 
-    echo "Activity 4\n";
+    echo "<br> <br>";
+
+    echo "Activity 4";
+    echo "<br>";
     for ($i = 1; $i <= 10; $i++) {
         if ($i == 5) {
             continue;
@@ -57,22 +63,24 @@
         }
     }
 
-    // CLI line break
-    echo "\n\n\n";
 
-    echo "Activity 5\n";
+    echo "<br> <br>";
+
+    echo "Activity 5";
+    echo "<br>";
     $addend = 1;
     $sum = 0;
     while ($addend <= 100) {
         $sum += $addend;
         $addend++;
     }
-    echo "The sum of numbers from 1 to 100 is: $sum \n";
+    echo "The sum of numbers from 1 to 100 is: $sum <br>";
 
-    // CLI line break
-    echo "\n\n";
 
-    echo "Activity 6\n";
+    echo "<br> <br>";
+
+    echo "Activity 6";
+    echo "<br>";
     $movies = array("Avatar", "Avengers: Endgame", "Avatar: The Way of Water", "Titanic", "Star Wars: Episode VII - The Force Awakens");
     $i = 1;
     foreach ($movies as $movie) {
@@ -80,31 +88,34 @@
         $i++;
     }
 
-    // CLI line break
-    echo "\n\n";
 
-    echo "Activity 7\n";
+    echo "<br> <br>";
+
+    echo "Activity 7";
+    echo "<br>";
     $userInfo = array("Name" => "Alice", "Age" => 20, "Grade" => "A", "City" => "Baguio");
     foreach ($userInfo as $info => $value) {
-        echo $info . ": " . $value . "\n";
+        echo $info . ": " . $value . "<br>";
     }
 
-    // CLI line break
-    echo "\n\n";
 
-    echo "Activity 8\n";
+    echo "<br> <br>";
+
+    echo "Activity 8";
+    echo "<br>";
     $numberFactor = 5;
     $factorial = 1;
     
     for ($i = $numberFactor; $i > 0; $i--) {
         $factorial = $factorial * $i;
     }
-    echo "The factorial of ". $numberFactor . " is: ". $factorial . "\n";
+    echo "The factorial of ". $numberFactor . " is: ". $factorial . "<br>";
 
-    // CLI line break
-    echo "\n\n";
 
-    echo "Activity 9\n";
+    echo "<br> <br>";
+
+    echo "Activity 9";
+    echo "<br>";
     for ($i = 1; $i <= 50; $i++) {
         if ($i % 3 == 0 && $i % 5 == 0) {
             echo "FizzBuzz ";
@@ -117,34 +128,34 @@
         }
     }
 
-    // CLI line break
-    echo "\n\n\n";
 
-    echo "Activity 10\n";
+    echo "<br> <br>";
+
+    echo "Activity 10<br>";
     
-    $userNumber = (int)readline("Enter a number: \n");
+    $userNumber = (int)readline("Enter a number: <br>");
     if ($userNumber  <= 1) {
-        echo $userNumber . " is not a prime number. \n";
+        echo $userNumber . " is not a prime number. <br>";
     }
 
     if ($userNumber  == 2 | $userNumber  == 3) {
-        echo $userNumber . " is a prime number. \n";
+        echo $userNumber . " is a prime number. <br>";
     }
 
     for ($i = 2; $i <= sqrt($userNumber); $i++) {     
         if ($userNumber % $i == 0) {
-            echo $userNumber . " is not a prime number. \n";
+            echo $userNumber . " is not a prime number. <br>";
             break;
         } else {
-            echo $userNumber . " is a prime number. \n";
+            echo $userNumber . " is a prime number. <br>";
             break;
         }
     }
 
-    // CLI line break
-    echo "\n\n";
 
-    echo "Activity 11\n";
+    echo "<br> <br>";
+
+    echo "Activity 11<br>";
     $subsequentNumber = 1;
     $sum = 0;
     $i = 1;
@@ -156,16 +167,17 @@
         $i++;
     }
 
-    // CLI line break
-    echo "\n\n\n";
 
-    echo "Activity 12\n";
-    $userString = (String)readline("Input: \n");
+    echo "<br> <br>";
+
+    echo "Activity 12<br>";
+    // $userString = (String)readline("Input: <br>");
+    $userString = "palindrome";
     $reversedString = "";
     for ($i = strlen($userString) - 1; $i >= 0; $i--) {
         $reversedString .= $userString[$i];
     }
-    echo "Output: " . $reversedString . "\n";
+    echo "Output: " . $reversedString . "<br>";
     ?>
 </body>
 </html>
